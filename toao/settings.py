aa638cb2955ce16b3b03a7bc28201a7d01eb2f4b
Akbar Sabri <hussain.akbarsabri@gmail.com>
2023-05-09 17:38:01 +0500

    database configuration added

--- toao/settings.py
+++ toao/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'toao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toao_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
